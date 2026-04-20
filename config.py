77d1e5d546a5db45d0c0019a7989f987d463d1e5
aagneye-syam <saagneye2003@gmail.com>
2026-04-20 21:27:47 +0530

    config and folder struct added

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+batch_size = 32
+block_size = 128
+max_iters = 5000
+eval_interval = 200
+learning_rate = 3e-4
+device = "cuda" 
+
+n_embd = 128
+n_head = 4
+n_layer = 4
+dropout = 0.2
